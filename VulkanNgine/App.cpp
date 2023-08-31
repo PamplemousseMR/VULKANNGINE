2d883e219150bbba8a8e2558327cd422eaf1a8e8
Romain Manciaux <romain.manciaux@gmail.com>
2023-08-31 11:33:32 +0200

    [feat] check and manage extensions and layers

--- VulkanNgine/App.cpp
+++ VulkanNgine/App.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <stdexcept>
 
+const std::vector<std::string> App::s_VALIDATION_LAYERS = {"VK_LAYER_KHRONOS_validation"};
+
 void App::run()
 {
     initWindow();
@@ -42,15 +44,37 @@ void App::createInstance()
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.apiVersion = VK_API_VERSION_1_0;
 
-    // Needed extenstions
+    // Needed extensions
     uint32_t glfwExtensionCount = 0;
     const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    std::vector<std::string> requiredExtensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
 
-    for(uint32_t i = 0; i < glfwExtensionCount; ++i)
+    for(const std::string& ex : requiredExtensions)
     {
-        std::cout << "Needed extension " << i << ": " << glfwExtensions[i] << std::endl;
+        std::cout << "Required extension " << ex << std::endl;
     }
 
+    // Check required extensions
+    if(!checkExtensionSupport(requiredExtensions))
+    {
+        throw std::runtime_error("One or more required extensions is not available");
+    }
+
+    // Check layers
+    if(s_ENABLE_VALIDATION_LAYERS)
+    {
+        for(const std::string& la : s_VALIDATION_LAYERS)
+        {
+            std::cout << "Required layer " << la << std::endl;
+        }
+    }
+
+    if(s_ENABLE_VALIDATION_LAYERS && !checkValidationLayerSupport(s_VALIDATION_LAYERS))
+    {
+        throw std::runtime_error("One or more required layers is not available");
+    }
+
+    // Instance information
     VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     createInfo.pApplicationInfo = &appInfo;
@@ -58,23 +82,84 @@ void App::createInstance()
     createInfo.ppEnabledExtensionNames = glfwExtensions;
     createInfo.enabledLayerCount = 0;
 
-    // Available extensions
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::vector<VkExtensionProperties> extensions(extensionCount);
-
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
-    for(const VkExtensionProperties& ex : extensions)
-    {
-        std::cout << "Available extension: " << ex.extensionName << " " << ex.specVersion << std::endl;
-    }
-
     // Create the instance
     if(vkCreateInstance(&createInfo, nullptr, &m_instance) != VK_SUCCESS)
     {
         throw std::runtime_error("failed to create instance!");
     }
+}
+
+bool App::checkExtensionSupport(const std::vector<std::string>& _extensions) const
+{
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::vector<VkExtensionProperties> availableExtensions(extensionCount);
+
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, availableExtensions.data());
+    for(const VkExtensionProperties& ex : availableExtensions)
+    {
+        std::cout << "Available extension: " << ex.extensionName << " " << ex.specVersion << std::endl;
+    }
+
+    for(const std::string& extensionName : _extensions)
+    {
+        bool extensionFound = false;
+
+        for(const auto& ex : availableExtensions)
+        {
+            if(strcmp(extensionName.c_str(), ex.extensionName) == 0)
+            {
+                extensionFound = true;
+                break;
+            }
+        }
+
+        if(!extensionFound)
+        {
+            std::cerr << "Extension: " << extensionName << " not available" << std::endl;
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool App::checkValidationLayerSupport(const std::vector<std::string>& _layers) const
+{
+    // Available layer
+    uint32_t layerCount = 0;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+    for(const VkLayerProperties& la : availableLayers)
+    {
+        std::cout << "Available layer: " << la.layerName << " " << la.specVersion << std::endl;
+    }
+
+    for(const std::string& layerName : _layers)
+    {
+        bool layerFound = false;
+
+        for(const auto& la : availableLayers)
+        {
+            if(strcmp(layerName.c_str(), la.layerName) == 0)
+            {
+                layerFound = true;
+                break;
+            }
+        }
+
+        if(!layerFound)
+        {
+            std::cerr << "Layer: " << layerName << " not available" << std::endl;
+            return false;
+        }
+    }
+
+    return true;
 }
 
 void App::mainLoop()
--- VulkanNgine/App.hpp
+++ VulkanNgine/App.hpp
@@ -14,11 +14,23 @@ class App
 
     static constexpr int s_HEIGHT = 600;
 
+    static const std::vector<std::string> s_VALIDATION_LAYERS;
+
+#ifdef NDEBUG
+    static constexpr bool s_ENABLE_VALIDATION_LAYERS = false;
+#else
+    static constexpr bool s_ENABLE_VALIDATION_LAYERS = true;
+#endif
+
     void initWindow();
 
     void initVulkan();
 
     void createInstance();
+
+    bool checkExtensionSupport(const std::vector<std::string>& _extensions) const;
+
+    bool checkValidationLayerSupport(const std::vector<std::string>& _validationLayers) const;
 
     void mainLoop();
 
